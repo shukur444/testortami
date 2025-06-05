6beac7aeb068b32c98ff77c898eff8b9ad27d120
Shukur <shukurmahmudlu71@gmail.com>
2025-06-05 14:36:40 +0400

    sadS

--- backend.tf
+++ backend.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "s3" {
-    bucket        = "yetims3"
-    key           = "env/dev/terraform.tfstate"
-    region        = "us-east-1"
-    encrypt       = true
-    workspace_lock_table = "terraform-locks"
+    bucket                = "yetims3"
+    key                   = "env/dev/terraform.tfstate"
+    region                = "us-east-1"
   }
 }
-
-
