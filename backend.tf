03c868b3f870c6b37c9b51af8aa7f932872140ba
Shukur <shukurmahmudlu71@gmail.com>
2025-06-05 12:35:15 +0400

    backend commitlendi

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket                = "yetims3"
+    key                   = "env/dev/terraform.tfstate"
+    region                = "us-east-1"
+    encrypt               = true
+    use_lockfile          = true
+    workspace_lock_table  = "terraform-locks"
+  }
+}
+
